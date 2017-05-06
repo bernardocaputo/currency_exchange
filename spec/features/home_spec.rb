2c3fd6c661d0ee788b011d765ef8bcc53739df17
bernardocaputo <bernardocaputo@gmail.com>
2017-05-06 01:39:45 -0700

    new background and opacity

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -1,33 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe "Exchange Currency Process", :type => :feature do
-  xit "exchange value" do
-    visit '/'
-    within("#exchange_form") do
-      select('EUR', from: 'currency')
-      select('USD', from: 'currency_destination')
-      fill_in 'quantity', with: '10'
-    end
-
-
-    save_and_open_page
-    expect(page).to have_content("value")
-  end
-
-  xit "switch currencies" do
-    visit '/'
-    within("#exchange_form") do
-      select('EUR', from: 'currency')
-      select('USD', from: 'currency_destination')
-      fill_in 'quantity', with: '10'
-    end
-    # click_button 'seta'
-
-    save_and_open_page
-
-    expect(page).to have_select('currency', selected: 'USD')
-    expect(page).to have_select('currency_destination', selected: 'EUR')
-  end
-
+  # it "exchange value" do
+  #   visit '/'
+  #   within("#exchange_form") do
+  #     select('EUR', from: 'currency')
+  #     select('USD', from: 'currency_destination')
+  #     fill_in 'quantity', with: '10'
+  #   end
+  #   click_button 'CONVERTER'
+  #
+  #   save_and_open_page
+  #   expect(page).to have_content("value")
+  # end
+  #
+  # xit "switch currencies" do
+  #   visit '/'
+  #   within("#exchange_form") do
+  #     select('EUR', from: 'currency')
+  #     select('USD', from: 'currency_destination')
+  #     fill_in 'quantity', with: '10'
+  #   end
+  #   click_button 'switch currency'
+  #
+  #   save_and_open_page
+  #
+  #   expect(page).to have_select('currency', selected: 'USD')
+  #   expect(page).to have_select('currency_destination', selected: 'EUR')
+  # end
+  #
 
 end
